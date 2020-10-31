887aed1614bfe7204e0296c68e2f2e0b6939b0af
Du Guoxin <anton.dgx@gmail.com>
2020-10-31 22:58:36 +0800

    added some properties seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning database..."
+Property.destroy_all
+Booking.destroy_all
+
+puts "Creating properties..."
+20.times do
+property = Property.new(
+  title: Faker::Address.street_name,
+  description: Faker::Lorem.paragraphs(number: 1).first,
+  address: Faker::Address.full_address,
+  price: rand(50..100),
+  capacity: rand(1..4),
+  user_id: 1
+)
+property.save
+puts "Created #{property.title}"
+end
+puts "Finished!"
+
+    # t.string "title"
+    # t.text "description"
+    # t.string "address"
+    # t.integer "price"
+    # t.integer "capacity"
