a81af667da0dce8062edb1806a215c1f24f1eec7
Du Guoxin <anton.dgx@gmail.com>
2020-11-01 23:34:36 +0800

    skip_before_action :authenticate_user! for show action too

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @properties = Property.all
