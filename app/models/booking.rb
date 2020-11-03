e0fd4c3e1b3b5c88e8a4d370598f23d235581c1e
adrien goux <adriengoux@gmail.com>
2020-11-03 11:33:17 +0800

    adding validation to property and booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :property
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user_id, presence: true
+  validates :property_id, presence: true
+  validates :price, presence: true
 end
