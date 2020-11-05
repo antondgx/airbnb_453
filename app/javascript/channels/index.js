ddc3de097c7ebea049e7c08be6a5a1887ceb3dce
adrien goux <adriengoux@gmail.com>
2020-11-05 20:56:33 +0800

    show/map

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,55 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+import mapboxgl from 'mapbox-gl';
+
+
+const baseUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/`;
+mapboxgl.accessToken = mapBoxKey;
+
+const form = document.querySelector('form');
+const input = document.querySelector('#location');
+const results = document.querySelector('#result');
+
+const drawMap = (long, lat) => {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [29.030378, 41.144889],
+    zoom: 12
+  });
+  new mapboxgl.Marker()
+    .setLngLat([29.030378, 41.144889])
+    .addTo(map);
+};
+
+const displayResults = (long, lat) => {
+  results.innerHTML = '';
+  const list = `<li class="list-inline-item">Long =${long}</li>
+                <li class="list-inline-item">Lat = ${lat}</li>`;
+  results.insertAdjacentHTML("beforeend", list);
+  drawMap(long, lat);
+};
+
+const getLongLat = (data) => {
+  const longitude = data.features[0].geometry.coordinates[0];
+  const latitude = data.features[0].geometry.coordinates[1];
+  displayResults(longitude, latitude);
+};
+
+const callMapBox = (search) => {
+  fetch(`${baseUrl}${search}.json?access_token=${mapBoxKey}`)
+    .then(response => response.json())
+    .then(getLongLat)
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  callMapBox(input.value);
+};
+
+form.addEventListener('submit', handleSubmit);
