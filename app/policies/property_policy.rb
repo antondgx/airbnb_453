641bca57ec5492dcd6500f4e71959b8fb250f3b6
Du Guoxin <anton.dgx@gmail.com>
2020-11-06 02:50:56 +0800

    add admin user and pundit

--- app/policies/property_policy.rb
+++ app/policies/property_policy.rb
@@ -14,10 +14,10 @@ class PropertyPolicy < ApplicationPolicy
   end
 
   def update?
-    user == record.user
+    user == record.user || user.admin
   end
 
   def destroy?
-    user == record.user
+    user == record.user || user.admin
   end
 end
