f86820480d9dc3fb5190b81f31c5488808f655d1
adrien goux <adriengoux@gmail.com>
2020-11-07 12:19:04 +0800

    modify seed to fit Geocoder address requirements

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_many_attached :photos
   belongs_to :user
   has_many :bookings, dependent: :destroy
