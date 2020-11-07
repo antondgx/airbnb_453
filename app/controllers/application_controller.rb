ffc4f23c997c24196c47e18996c92769b2aaaa0a
adrien goux <adriengoux@gmail.com>
2020-11-07 16:21:27 +0800

    add link to user account. adding avatar afterward does not work yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(account_update: [:password, :password_confirmation, :current_password, :avatar])
   end
 
   private
