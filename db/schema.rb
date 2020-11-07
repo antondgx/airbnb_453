e021c189d4ef05d32093b8b622b834e959f2686b
Summer S <summer.s2020@icloud.com>
2020-11-07 17:26:47 +0900

    work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_033125) do
+ActiveRecord::Schema.define(version: 2020_11_07_043507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
