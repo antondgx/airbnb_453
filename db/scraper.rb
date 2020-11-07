c317386c6e3c8cd6442e107df479597be1d03541
Du Guoxin <anton.dgx@gmail.com>
2020-11-07 18:07:04 +0800

    remove nokogiri

--- db/scraper.rb
+++ db/scraper.rb
@@ -1,11 +1,11 @@
-require "open-uri"
-require "nokogiri"
+# require "open-uri"
+# require "nokogiri"
 
-def scrape_address
-  url = "https://www.fakeaddressgenerator.com/All_countries/address/country/Singapore"
-  html_file = open(url).read
-  html_doc = Nokogiri::HTML(html_file)
-  # get the the first url in the list
-  # guard against crash
-  address = html_doc.search(".no-style")[4].attribute("value")
-end
+# def scrape_address
+#   url = "https://www.fakeaddressgenerator.com/All_countries/address/country/Singapore"
+#   html_file = open(url).read
+#   html_doc = Nokogiri::HTML(html_file)
+#   # get the the first url in the list
+#   # guard against crash
+#   address = html_doc.search(".no-style")[4].attribute("value")
+# end
