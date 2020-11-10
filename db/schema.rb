3c5e1bb98e883be38dc19bfbb41ece25be6a5565
yuva k <yuvarajan1989@gmail.com>
2020-11-10 21:49:47 +0800

    created payment_features branch

--- db/schema.rb
+++ db/schema.rb
@@ -81,8 +81,8 @@ ActiveRecord::Schema.define(version: 2020_11_07_065233) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
     t.string "username"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
