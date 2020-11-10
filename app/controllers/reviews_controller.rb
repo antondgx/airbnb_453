1f9f7fb782bf0555b29b8254ff5d9b3558346701
adrien goux <adriengoux@gmail.com>
2020-11-10 19:44:33 +0800

    reviews - adding stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,22 @@
 class ReviewsController < ApplicationController
   before_action :find_property
   skip_before_action :authenticate_user!, only: [:index, :show]
-  def new 
-    @review = Review.new 
+  def new
+    @review = Review.new
     authorize @review
-  end 
+  end
 
   def create
     @review = Review.new(review_params)
     @review.user = current_user
     @review.property = @property
     authorize @review
-    if @review.save 
+    if @review.save
       redirect_to property_path(@property)
     else
       flash[:alert] = "Something went wrong."
       render :new
-    end 
+    end
   end
 
   private
@@ -25,7 +25,7 @@ class ReviewsController < ApplicationController
     @property = Property.find(params[:property_id])
   end
 
-  def review_params 
+  def review_params
     params.require(:review).permit(:content, :rating)
-  end 
+  end
 end
