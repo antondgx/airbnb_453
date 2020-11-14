5b7c091362b589e97630f7fe19284bf282e9214f
adrien goux <adriengoux@gmail.com>
2020-11-14 12:09:24 +0800

    remove comment on index method

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,8 +3,6 @@ class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @properties = Property.all
-    # @properties = policy_scope(Property).order(created_at: :desc)
     if params[:query].present?
       @properties = policy_scope(Property).search_by_title_description_and_address(params[:query])
     else
